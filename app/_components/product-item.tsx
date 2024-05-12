690ff38a0b8022725608e6a44849b771d69c233b
Erico Ferreira <eriicoalan@gmail.com>
2024-05-12 14:56:13 -0300

    feat: add product list and item components

---
+++ app/_components/product-item.tsx
@@ -0,0 +1,57 @@
+import { Prisma } from "@prisma/client";
+import Image from "next/image";
+import { calculateProductTotalPrice, formatCurrency } from "./_helpers/price";
+import { ArrowDownIcon } from "lucide-react";
+
+interface ProductItemProps {
+    product: Prisma.ProductGetPayload<{
+        include: {
+            restaurant: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    }>
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+    return (
+        <div className="space-y-2 w-[150px] min-w-[150px]">
+            <div className="relative h-[150px] w-full">
+                <Image 
+                    src={product.imageUrl} 
+                    alt={product.name} 
+                    fill 
+                    className="object-cover rounded-lg shadow-md"
+                />
+
+                {product.discountPercentage && (
+                    <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-1 py-[2px] text-white">
+                        <ArrowDownIcon size={12} />
+                        <span className="text-xs font-semibold">
+                            {product.discountPercentage}%
+                        </span>
+                    </div>
+                )}
+            </div>
+            <div>
+                <h2 className="text-sm truncate">{product.name}</h2>
+                <div className="flex gap-1 items-center">
+                    <h3 className="font-semibold">
+                        {formatCurrency(calculateProductTotalPrice(product))}
+                    </h3>
+                    {product.discountPercentage > 0 && (
+                        <span className="text-muted-foreground line-through text-xs">
+                            {formatCurrency(Number(product.price))}
+                        </span>
+                    )}
+                </div>
+
+                <span className="block text-xs text-muted-foreground">{product.restaurant.name}</span>
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
+import ProductList from "./_components/product-list";
 import Search from "./_components/search";
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon, Ghost } from "lucide-react";
 
 const Home = () => {
   return (
@@ -25,6 +28,17 @@ const Home = () => {
         sizes="100vw"
         quality={100}
       />
+    </div>
+
+    <div className="pt-6 space-y-4">
+      <div className="px-5 flex items-center justify-between">
+        <h2 className="font-semibold">Pedidos Recomendados</h2>
+        <Button variant="ghost" className="text-primary p-0 hover:bg-transparent h-fit">
+          Ver todos
+          <ChevronRightIcon size={16}/>
+        </Button>
+      </div>
+      <ProductList />
     </div>
   </>
 )};
