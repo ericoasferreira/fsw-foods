5914b8839dfadd440fe17718f4b64bee040731ac
Erico Ferreira <eriicoalan@gmail.com>
2024-05-12 16:02:47 -0300

    feat: add restaurant list component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "./_components/ui/button";
 import { ChevronRightIcon, Ghost } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -58,6 +59,17 @@ const Home = async () => {
         src="/promo-banner-02.png" 
         alt="A partir de R$17,90 em lanches"
       />
+    </div>
+
+    <div className="py-6 space-y-4">
+      <div className="px-5 flex items-center justify-between">
+        <h2 className="font-semibold">Resturantes Recomendados</h2>
+        <Button variant="ghost" className="text-primary p-0 hover:bg-transparent h-fit">
+          Ver todos
+          <ChevronRightIcon size={16}/>
+        </Button>
+      </div>
+      <RestaurantList />
     </div>
   </>
 )};
